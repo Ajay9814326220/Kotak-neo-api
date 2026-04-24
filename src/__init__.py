465569360775092d3312c29f3f217a9b35256c53
Ajay9814326220 <sarpal.ajay@rediffmail.com>
2026-04-25 02:34:01 +0530

    Update __init__.py to export exceptions and improve interface

--- src/__init__.py
+++ src/__init__.py
@@ -4,7 +4,14 @@ __version__ = "1.0.0"
 __author__ = "Ajay9814326220"
 __description__ = "Python integration for Kotak Securities Neo API"
 
-from .kotak_client import KotakNeoClient
-from .auth import KotakAuth
+from .kotak_client import KotakNeoClient, ApiError
+from .auth import KotakAuth, AuthenticationError
+from .exceptions import KotakApiException
 
-__all__ = ["KotakNeoClient", "KotakAuth"]
+__all__ = [
+    "KotakNeoClient",
+    "KotakAuth",
+    "ApiError",
+    "AuthenticationError",
+    "KotakApiException"
+]
